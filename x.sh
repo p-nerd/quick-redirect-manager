94e8c484712b7f97843b183376ad42faa5de3479
Shihab Mahamud <shihab4t@gmail.com>
2024-12-28 23:02:59 +0600

    x zip

---
+++ x.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NAME="quick-redirect-manager.zip"
+
+if [ "$1" = "zip" ]; then
+    if [ -f "$NAME" ]; then
+        echo "[INFO] Removing old $NAME file..."
+        rm "$NAME"
+        echo "[SUCCESS] Removed old $NAME file."
+    fi
+
+    if [ -f ".zip.log" ]; then
+        rm ".zip.log"
+    fi
+
+    echo "[INFO] Install composer..."
+    composer install
+
+    echo "[INFO] Zipping the current directory..."
+    zip -r "$NAME" . > .zip.log
+    echo "[SUCCESS] Zipped the current directory and created $NAME"
+fi
